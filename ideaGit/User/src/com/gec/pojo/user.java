65c50c8bbfb87b0c777240ac22a0648963354488
newuser <https://github.com/whf924/gz2104.git>
2021-05-12 11:00:48 +0800

    坚实的福建师大

---
+++ ideaGit/User/src/com/gec/pojo/user.java
@@ -0,0 +1,12 @@
+package com.gec.pojo;
+
+public class user {
+
+    private static String username;
+    private static String password;
+
+    public static void main(String[] args) {
+        username = "我是大神仙";
+        password = "我叫时江";
+    }
+}
